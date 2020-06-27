a46e23f0067f72e4675178c48d6d03bf35dd5ec4
Sam-BCC <65135395+Sam-BCC@users.noreply.github.com>
2020-06-27 20:18:05 +0300

    Update DeployingSmartContracts.js

--- DeployingSmartContracts.js
+++ DeployingSmartContracts.js
@@ -2,15 +2,15 @@ var Tx = require('ethereumjs-tx')
 const Web3 = require('web3')
 const web3 = new Web3('https://ropsten.infura.io/YOUR_INFURA_API_KEY')
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
+const account1 = '0x09E78Ec232C168ec6ef420675F813C4719A811f8'
 
-const privateKey1 = Buffer.from('YOUR_PRIVATE_KEY_1', 'hex')
-const privateKey2 = Buffer.from('YOUR_PRIVATE_KEY_2', 'hex')
+const privateKey1 = Buffer.from('3D84D21DD7B6AC5F0B3BFEB35BB05ACB4F70F7B462AAB72B531F6D78B090486E', 'hex')
+
 
 // Deploy the contract
 web3.eth.getTransactionCount(account1, (err, txCount) => {
-  const data = ''
+  const data = '0x608060405234801561001057600080fd5b5060c78061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80636057361d146037578063b05784b8146062575b600080fd5b606060048036036020811015604b57600080fd5b8101908080359060200190929190505050607e565b005b60686088565b6040518082815260200191505060405180910390f35b8060008190555050565b6000805490509056fea2646970667358221220be5823d50fd48ab58b3251892c327ef78ae8f414e32e6783edc8c0a7c9c3699364736f6c63430006060033', 
+'
 
 const txObject = {
     nonce:    web3.utils.toHex(txCount),
